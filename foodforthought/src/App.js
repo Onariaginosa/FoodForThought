9aa2bbc7056adca40bad3819b2e2c3b9f530b3f1
warren <lee@Lees-MacBook-Pro.local>
2020-02-27 18:49:32 -0800

    foodform

--- foodforthought/src/App.js
+++ foodforthought/src/App.js
@@ -1,21 +1,32 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from 'react-bootstrap/Navbar';
+import foodform from "./foodform.js";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from "react-router-dom";
+
+
 import './App.css';
 
 function App() {
   return (
-    <div style={{backgroundColor: "#e3e3e3"}}>
-      <Navbar style={{ backgroundColor: 'white', color: 'black' }}>
-        <Navbar.Brand >Food For Thought</Navbar.Brand>
-        <Navbar.Toggle />
-      </Navbar>
-      <text>
-        hello
-      </text>
-
-
-
+    <div>
+      <div>
+        <Navbar style={{ backgroundColor: 'white', color: 'black' }}>
+          <Navbar.Brand >Food For Thought</Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar>
+      </div>
+      <div>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={foodform}></Route>
+          </Switch>
+        </Router>
+      </div>
     </div>
   );
 }
---
+++ foodforthought/src/foodform.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import ReactMultiSelectCheckboxes from 'react-multiselect-checkboxes';
+
+
+import './App.css';
+
+const DietaryRestrictions = [
+  { label: 'Dietary Restriction 1', value: 1},
+  { label: 'Dietary Restriction 2', value: 2},
+];
+
+const Intolerances = [
+  { label: 'Intolerance 1', value: 1},
+  { label: 'Intolerance 2', value: 2},
+];
+
+function foodform() {
+  return (
+    <div style={{backgroundColor: "#e3e3e3"}}>
+
+
+      <ul> 
+        {
+          <label>Food Form</label>
+        }
+        {      
+          <form>
+            <label>
+              Dietary Restrictions:
+              <ReactMultiSelectCheckboxes options={DietaryRestrictions} />
+            </label>
+          </form>
+        } 
+        {      
+          <form>
+            <label>
+              Intolerances:
+              <ReactMultiSelectCheckboxes options={Intolerances} />
+            </label>
+            
+          </form>
+        } 
+        {      
+          <form>
+            <label>
+              Search Ingredients:
+              <input type="text" name="name" />
+            </label>
+            
+          </form>
+        } 
+        {
+          <input type="submit" value="Submit" />
+        }
+      </ul>
+
+
+
+    </div>
+  );
+}
+
+export default foodform;
